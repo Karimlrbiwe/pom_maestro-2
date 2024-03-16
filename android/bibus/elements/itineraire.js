671d96896a14ff82cbf4913eb4862ee47ab37289
kareem92 <karimlarbi.unice@gmail.com>
2024-03-16 15:03:53 +0100

    projet qui marche pour twisto, essayons de rajouter une app

---
+++ android/bibus/elements/itineraire.js
@@ -0,0 +1,6 @@
+output.itineraire= {
+    origine: "gare de brest",
+    slc_origine: "BIBUS, Office de Tourisme de Brest Métropole,",
+    destination: "memorial americain",
+    slc_destination: "memorial americain"
+}
---
+++ android/twisto/elements/itineraire.js
@@ -0,0 +1,6 @@
+output.itineraire= {
+    origine: "gare de caen",
+    slc_origine: "Lieux, Gare SNCF, Caen (14000), transports disponibles : bus",
+    destination: "memorial de caen",
+    slc_destination: "Adresse, Mémorial de Caen,  Esplanade Général Eisenhower, 14000, Caen"
+}
