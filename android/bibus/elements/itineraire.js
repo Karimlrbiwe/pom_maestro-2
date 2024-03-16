f87e6a7e342c69bb24a00b7ee8faa26b20909c97
kareem92 <karimlarbi.unice@gmail.com>
2024-03-16 19:54:29 +0100

    projet fonctionnel pour 3 apps, avec recherche itineraire et recherche de ligne, mais fichier a modifier

--- android/bibus/elements/itineraire.js
+++ android/bibus/elements/itineraire.js
@@ -1,6 +1,13 @@
 output.itineraire= {
+    // Recherche itineraire
     origine: "gare de brest",
-    slc_origine: "BIBUS, Office de Tourisme de Brest Métropole,",
+    slc_origine: "BIBUS, Office de Tourisme de Brest Métropole, ",
     destination: "memorial americain",
-    slc_destination: "memorial americain"
+    slc_destination: "Adresse, Mémorial Américain (1914-1918), Brest (29200)",
+    app_name: "fr.bibus.android",
+
+    // Recherhe ligne
+    nom_ligne: "30",
+    resultat_ligne_attendu: "Ligne 30, NOCTYBUS Gascogne - Les Iles, BIBUS",
+    arret_1_ligne_attendu : "Les Îles"
 }
---
+++ android/ondea/elements/itineraire.js
@@ -0,0 +1,14 @@
+output.itineraire= {
+    // Recherche itineraire
+    origine: "mon revard",
+    slc_origine: "Adresse, Mont Revard, Les Déserts (73230)",
+    destination: "jardin vagabond",
+    slc_destination: "Adresse, Jardin Vagabond, Aix-les-Bains (73100)",
+    app_name: "com.is.android.alb",
+
+    // Recherhe ligne
+    nom_ligne: "11",
+    resultat_ligne_attendu: "Ligne 11, Collège Garibaldi - ESAT, AIX-LES-BAINS - Ondéa",
+    arret_1_ligne_attendu : "Collège Garibaldi"
+
+}
